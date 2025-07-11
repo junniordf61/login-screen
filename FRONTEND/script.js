1a3252039137d992b9196bdd3255d1aef776116e
junniordf61 <junniordf61@gmail.com>
2025-07-11 17:43:14 -0300

    novo script

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -54,6 +54,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Erro interno do servidor');
 });
 
+
 //Listar usuários
 
 app.get('/usuarios', async (req, res) => {
@@ -68,6 +69,40 @@ app.get('/usuarios', async (req, res) => {
   }
 });
 
+
+
+
+
+
+app.post('/usuarios', async (req, res) => {
+  const { name, email, senha } = req.body;
+
+  try {
+    // Verifica se já existe um email igual
+    const existente = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (existente.rows.length > 0) {
+      return res.status(400).json({ erro: 'Email já cadastrado.' });
+    }
+
+    // Criptografa a senha
+    const passwordHashed = await bcrypt.hash(senha, 10);
+
+    const result = await db.query(
+      'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email, created_at',
+      [name, email, passwordHashed]
+    );
+
+    return res.status(201).json({
+      mensagem: 'User successfully registered',
+      usuario: result.rows[0]
+    });
+  } catch (error) {
+    return res.status(500).json({
+      erro: 'Erro ao cadastrar usuário',
+      detalhe: error.message
+    });
+  }
+});
 
 
 app.post('/login', async (req, res) => {
